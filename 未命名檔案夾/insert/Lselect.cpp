f7f93c693a3b6fca3b7f1bb019c2a1229aaf9848
lianghandsome <145639153+lianghandsome@users.noreply.github.com>
2023-11-20 17:34:38 +0800

    Add files via upload

---
+++ 未命名檔案夾/insert/Lselect.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a[1000];
+	int n;
+	cin>>n;
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	
+	for(int i=0;i<n;i++){
+		for(int j=i;j>0;j--){
+			if(a[j]>a[j-1])
+				swap(a[j-1],a[j]);
+		}
+	}
+
+	for(int i=0;i<n;i++)
+		cout<<a[i]<<" ";
+} 
